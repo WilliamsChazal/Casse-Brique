9cfdbd363ea0a806fa3cc9c36f5085850021db07
williams <w.chazal@codeur.online>
2021-07-23 16:41:41 +0200

    ball qui rebondit mais pb raquette qui apparait pas

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ Définir une boucle de dessin
 Afin que le dessin soit mis à jour sur le canevas à chaque image, nous allons définir une fonction draw() qui sera exécutée en continu et qui utilisera des variables pour les positions des sprites, etc. Pour qu'une fonction s'exécute de façon répétée avec JavaScript, on pourra utiliser les méthodes setInterval() (en-US) ou requestAnimationFrame().
 
 Grâce à la nature infinie de setInterval, la fonction draw() sera appelée toutes les 10 millisecondes, sans arrêt jusqu'à ce que nous y mettions un terme. 
+
+La balle laisse une trace parce que qu'une nouveau cercle est dessiné sur chaque frame sans qu'on enlève le précédent. Pas d'inquiétude, il existe un moyen d'effacer le contenu du canevas : clearRect(). Cette méthode prend en compte quatre paramètres: les coordonnées x et y du coin supérieur gauche d'un rectangle et les coordonnées x et y du coin inférieur droit d'un rectangle. Toute la zone couverte par ce rectangle sera effacée.
+
+Toutes les 10 millisecondes, le canvas est effacé, la balle est dessinée sur une position donnée et les valeurs x et y sont mises à jour pour l'image suivante.
+
+Rebondir en haut et en bas
+Il y a 4 murs en tout mais nous allons d'abord nous pencher sur le mur du haut. Nous devons, à chaque rafraichissement du canvas, regarder si la balle touche le bord du haut. Si c'est le cas, alors nous devons inverser la direction de la balle pour créer un effet de limite de zone de jeu. Il ne faut surtout pas oublier que le point d'origine est en haut à gauche !
--- main.js
+++ main.js
@@ -1,23 +1,57 @@
 const canvas =document.getElementById("myCanvas")
 const ctx =canvas.getContext("2d")
+const ballRadius = 20; // Variable qui contient le rayon du cercle dessiné et sera utilisée pour les calculs. 
+const paddleHeight = 10;
+const paddleWidht =75;
+const paddleX =(canvas.width-paddleWidht/2); // Variable point de départ de la raquette 
 
 let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
-let dy = -2;
+let dy = -8;
 
-function draw() {
-    // le code pour dessiner
+//Fonction de dessin de la raquette
+function drawPaddle() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleHeight, paddleWidht);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+  }
+    
+
+
+
+
+//Fonction de dessin de la balle
+function drawBall() {
+    // le code pour dessiner
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+// Fonctions pour effacer le canvas avant chaque frame
+function draw(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    drawBall(); 
     x += dx;
     y += dy;
-
-  }
-  setInterval(draw, 10); // la fonction draw() sera exécutée toutes les 10 millisecondes. 
+    // les IF qui suivent permettent de faire rebondire la balle sur les 4 bords du canvas rajouter la variable ballRadius permet de calculer par rapport au radius de la ball et non au centre de la ball avec <0
+    if (y + dy > canvas.height-ballRadius || y +dy < ballRadius) {
+        dy = -dy;
+        ctx.fillStyle ="red" ;
+        ctx.fill()  
+    }
+    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx
+        ctx.fillStyle ="red" ;
+        ctx.fill();
+            
+    }
+}
+  setInterval(draw, 20); // la fonction draw() sera exécutée toutes les 10 millisecondes. 
 
 /* // Premier chemin
 ctx.beginPath(); 
